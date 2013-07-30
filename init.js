67efcf72fb754277e081c76808b5f9c79fdb8bf4
TheBigSadowski <joe.sadowski@gmail.com>
2013-07-30 15:36:05 -0700

    adding more detial to the listing of nearby segments

--- init.js
+++ init.js
@@ -1,4 +1,4 @@
-var id = $('.starred:first').data('segment-id');
+//var id = $('.starred:first').data('segment-id');
 
 var searchForSegments = function(bounds, callback) {
 	//TODO: add segment type recognition
@@ -14,28 +14,25 @@ var searchForSegments = function(bounds, callback) {
 	});
 };
 
+var metersToMiles = function(distance) {
+	return distance * 0.000621371;
+}
+
 var formatSegment = function(s) {
 	return '' +
-		'<li><a href="/segments/'+s.id+'">'+s.name+'</a></li>' + 
-		''; 
+		'<li>' +
+		'<a href="/segments/'+s.id+'">'+s.name+'</a>' + 
+		'<br>' +
+		'<span>'+metersToMiles(s.distance).toFixed(1)+' mi</span> <span>'+s.avg_grade.toFixed(1)+'%</span>' +
+		'</li>'; 
 };
 
 var formatModule = function(segments) {
 	return '' +
-		'<div class="module">' +
+		'<div class="module nearby-segments">' +
 		'<h4>Nearby Segments</h4>' +
 		'<ul>' +
-		_.map(segments.segments, formatSegment).join('') +
-		'</ul>' +
-		'</div>' +
-		'';
-};
-
-var tst = function() {
-	return '' +
-		'<div class="module">' +
-		'<h4>Nearby Segments</h4>' +
-		'<ul>' +
+		_.map(segments, formatSegment).join('') +
 		'</ul>' +
 		'</div>' +
 		'';
@@ -54,35 +51,37 @@ var expand = function(bounds) {
 	}
 };
 
-var findNearbySegments = function(bounds, callback) {
-	var url = '/api/v3/segments/search?bounds='+bounds.lat.min+'%2C'+bounds.lng.min+'%2C'+bounds.lat.max+'%2C'+bounds.lng.max+'&zoom=15&min_cat=0&max_cat=5&activity_type=cycling';
-	$.get(url, function(results) {
-		var segments = results.segments;
-		if (results.segments.length < 3) {
-			findNearbySegments(expand(bounds), callback);
-			return;
+var loadNearbySegmentsSection = function(id) {
+
+	var findNearbySegments = function(bounds, callback) {
+		var url = '/api/v3/segments/search?bounds='+bounds.lat.min+'%2C'+bounds.lng.min+'%2C'+bounds.lat.max+'%2C'+bounds.lng.max+'&zoom=15&min_cat=0&max_cat=5&activity_type=cycling';
+		$.get(url, function(results) {
+			var segments = _.chain(results.segments)
+				.reject(function(s) { return s.id == id })
+				.take(4)
+				.value();
+			if (segments.length < 3) {
+				findNearbySegments(expand(bounds), callback);
+				return;
+			}
+			var content = formatModule(segments);
+			$('.sidebar .module:last').before(content);
+		});	
+	};
+
+	$.get('/stream/segments/'+id, function(data) {
+		var bounds = {
+			lat: {
+				min: _.chain(data.latlng).map(function(p) { return p[0]; }).min().value(),
+				max: _.chain(data.latlng).map(function(p) { return p[0]; }).max().value()
+			},
+			lng: {
+				min: _.chain(data.latlng).map(function(p) { return p[1]; }).min().value(),
+				max: _.chain(data.latlng).map(function(p) { return p[1]; }).max().value()
+			}
 		}
-		var content = formatModule(results.segments);
-		$('.sidebar .module:last').before(content);
-	});	
+		findNearbySegments(bounds);
+	});
 };
 
-$.get('/stream/segments/'+id, function(data) {
-	var bounds = {
-		lat: {
-			min: _.chain(data.latlng).map(function(p) { return p[0]; }).min().value(),
-			max: _.chain(data.latlng).map(function(p) { return p[0]; }).max().value()
-		},
-		lng: {
-			min: _.chain(data.latlng).map(function(p) { return p[1]; }).min().value(),
-			max: _.chain(data.latlng).map(function(p) { return p[1]; }).max().value()
-		}
-	}
-	findNearbySegments(bounds);
-//	var url = '/api/v3/segments/search?bounds='+bounds.lat.min+'%2C'+bounds.lng.min+'%2C'+bounds.lat.max+'%2C'+bounds.lng.max+'&zoom=15&min_cat=0&max_cat=5&activity_type=cycling';
-//	$.get(url, function(results) {
-//		var content = formatModule(results);
-//		$('.sidebar .module:last').before(content);
-//	});
-});
-//$('.sidebar .module:last').before('<div class="module"><h4>Nearby Segments</h4><ul><li><a href="#">Cool segment 1</a><li><a href="#">Another rad segment</a></ul></div>')
+loadNearbySegmentsSection($('.starred:first').data('segment-id'));

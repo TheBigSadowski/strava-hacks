ad0c5cd9f297152e7809de5acc042e773c75f5da
TheBigSadowski <joe.sadowski@gmail.com>
2013-07-30 13:53:21 -0700

    working plugin shell

---
+++ init.js
@@ -0,0 +1,4 @@
+var id = $('.starred:first').data('segment-id');
+
+
+$('.sidebar .module:last').before('<div class="module"><h4>Nearby Segments for '+id+'</h4><ul><li><a href="#">Cool segment 1</a><li><a href="#">Another rad segment</a></ul></div>')
